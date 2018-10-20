38daaffc9b26f1ae03d3259643b76e7f3f978a20
Kate <kduncanwelke@gmail.com>
2018-10-20 17:53:59 -0500

    UI setup and outlets

--- Bout Time/Bout Time/ViewController.swift
+++ Bout Time/Bout Time/ViewController.swift
@@ -9,17 +9,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var fact1: UIButton!
+    @IBOutlet weak var fact2: UIButton!
+    @IBOutlet weak var fact3: UIButton!
+    @IBOutlet weak var fact4: UIButton!
+    @IBOutlet weak var timer: UILabel!
+    @IBOutlet weak var learnMore: UILabel!
+    @IBOutlet weak var resultButton: UIButton!
+    
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var view2: UIView!
+    @IBOutlet weak var view3: UIView!
+    @IBOutlet weak var view4: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // round left-hand corners on views
+        roundCorners(view: view1, cornerRadius: 5.0)
+        roundCorners(view: view2, cornerRadius: 5.0)
+        roundCorners(view: view3, cornerRadius: 5.0)
+        roundCorners(view: view4, cornerRadius: 5.0)
+        
+        resetRound()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - Custom Functions
+    
+    func resetRound() {
+        // hide results and learn more buttons
+        resultButton.isHidden = true
+        learnMore.isHidden = true
+        
+        // disable buttons to go to web view
+        fact1.isEnabled = false
+        fact2.isEnabled = false
+        fact3.isEnabled = false
+        fact4.isEnabled = false
+    }
+    
+    func roundCorners(view: UIView, cornerRadius: Double) {
+        view.layer.cornerRadius = CGFloat(cornerRadius)
+        view.clipsToBounds = true
+        view.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMinXMinYCorner]
+    }
 
-
+    // MARK: - Actions
+    
+    // go to web view
+    @IBAction func factTap(_ sender: UIButton) {
+    }
+    
+    // go to next round
+    @IBAction func nextRound(_ sender: UIButton) {
+    }
+    
 }
 
