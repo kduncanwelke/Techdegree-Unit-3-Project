57202896c5ba5f0316bf5044926bc2381c06d8f4
Kate <kduncanwelke@gmail.com>
2018-10-23 15:32:37 -0500

    Code reorganization

--- Bout Time/Bout Time/ViewController.swift
+++ Bout Time/Bout Time/ViewController.swift
@@ -39,17 +39,14 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     
     // MARK: - Properties
+    
     var webView: WKWebView!
     
     var shuffledFacts = Fact.shuffleFacts()
     
     var factIndex = 0
     
-    // timer properties
-    var seconds = 60
-    var timer = Timer()
-    var isTimerOn = false
-    
+    // indexes to track current fact assigned to each button
     var fact1Index = 0
     var fact2Index = 0
     var fact3Index = 0
@@ -58,22 +55,32 @@ class ViewController: UIViewController, WKNavigationDelegate {
     var rounds = 0
     var correctAnswers = 0
     
+    // timer properties
+    var seconds = 60
+    var timer = Timer()
+    var isTimerOn = false
+    
+    
+    // sounds
     let correctSound = Sound(number: 0, resourceName: "CorrectDing", type: "wav")
     let incorrectSound = Sound(number: 1, resourceName: "IncorrectBuzz", type: "wav")
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        // load sounds
         Sound.loadSound(number: &correctSound.number, resourceName: correctSound.resourceName, type: correctSound.type)
         Sound.loadSound(number: &incorrectSound.number, resourceName: incorrectSound.resourceName, type: incorrectSound.type)
         
+        // set game counters to zero
         factIndex = 0
         rounds = 0
         correctAnswers = 0
         
+        // make sure buttons to access web are inactive
         disableWebButtons()
-        showFacts()
         
         // round left-hand corners on views
         roundCorners(view: view1, cornerRadius: 5.0)
@@ -81,7 +88,10 @@ class ViewController: UIViewController, WKNavigationDelegate {
         roundCorners(view: view3, cornerRadius: 5.0)
         roundCorners(view: view4, cornerRadius: 5.0)
         
-        // hide results and learn more buttons
+        // display facts
+        showFacts()
+        
+        // keep results and learn more buttons hidden until round is over
         resultButton.isHidden = true
         learnMore.isHidden = true
     }
@@ -91,10 +101,18 @@ class ViewController: UIViewController, WKNavigationDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    
     // MARK: UI Updates
     
+    func roundCorners(view: UIView, cornerRadius: Double) {
+        // round left-hand corners of views behind text to match rounded arrow buttons
+        view.layer.cornerRadius = CGFloat(cornerRadius)
+        view.clipsToBounds = true
+        view.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMinXMinYCorner]
+    }
+    
     func disableSwitchButtons() {
-        // disable fact switching
+        // disable fact switching buttons
         arrowButton1.isEnabled = false
         arrowButton2.isEnabled = false
         arrowButton3.isEnabled = false
@@ -104,6 +122,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
     }
     
     func enableSwitchButtons() {
+        // enable fact switching buttons
         arrowButton1.isEnabled = true
         arrowButton2.isEnabled = true
         arrowButton3.isEnabled = true
@@ -129,11 +148,28 @@ class ViewController: UIViewController, WKNavigationDelegate {
         fact4.isEnabled = true
     }
     
+    func hideItems() {
+        // hide items when displaying web view
+        stackView.isHidden = true
+        resultButton.isHidden = true
+        learnMore.isHidden = true
+    }
+    
+    func showItems() {
+        // show items again when web view is dismissed, hide web bar
+        stackView.isHidden = false
+        resultButton.isHidden = false
+        learnMore.isHidden = false
+        webviewBar.isHidden = true
+    }
+    
     func showFacts() {
+        // put buttons in array
        let buttons = [fact1, fact2, fact3, fact4]
         
         if let factArray = shuffledFacts {
             
+            // loop through buttons and assign text and temporary index to reference fact in array
             for button in buttons {
                 let currentFact = factArray[factIndex]
                 if let button = button {
@@ -155,25 +191,34 @@ class ViewController: UIViewController, WKNavigationDelegate {
                 }
             }
         }
+        
+        // show and run timer
         timerLabel.isHidden = false
         timerLabel.text = "0.\(seconds)"
         runTimer()
     }
     
+    func resetRound() {
+        // reset ui, turning off web buttons, enable arrow switching, show next fact
+        disableWebButtons()
+        enableSwitchButtons()
+        showFacts()
+        
+        // hide results and learn more buttons (will toggle on again when round ends)
+        resultButton.isHidden = true
+        learnMore.isHidden = true
+    }
+    
 
     // MARK: - Custom Functions
     
-    func roundCorners(view: UIView, cornerRadius: Double) {
-        view.layer.cornerRadius = CGFloat(cornerRadius)
-        view.clipsToBounds = true
-        view.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMinXMinYCorner]
-    }
-    
     override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
+        // respond to device shake motion
         showResult()
     }
     
     func checkAnswer() -> Bool? {
+        // check that each fact's year is less than the previous (BC years are represented as negative values)
         guard let factArray = shuffledFacts else { return nil }
         if factArray[fact1Index].year < factArray[fact2Index].year && factArray[fact2Index].year < factArray[fact3Index].year && factArray[fact3Index].year < factArray[fact4Index].year {
             rounds += 1
@@ -190,6 +235,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
     func showResult() {
         let answer = checkAnswer()
         
+        // change result button image based on correct/incorrect answer
         if let chosenAnswer = answer {
             if chosenAnswer {
                 resultButton.setImage(#imageLiteral(resourceName: "next_round_success"), for: .normal)
@@ -199,20 +245,14 @@ class ViewController: UIViewController, WKNavigationDelegate {
                 resultButton.isHidden = false
             }
         }
+        
+        // turn off button switching, as round is ended and user should no longer be able to change answer
         disableSwitchButtons()
+        
+        // hide timer, show learn more label, and enable buttons to web view
         learnMore.isHidden = false
         enableWebButtons()
         hideTimer()
-    }
-    
-    func resetRound() {
-        disableWebButtons()
-        enableSwitchButtons()
-        showFacts()
-        
-        // hide results and learn more buttons
-        resultButton.isHidden = true
-        learnMore.isHidden = true
     }
     
     // function for when timer is running
@@ -225,7 +265,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
         seconds -= 1
         timerLabel.text = "0.\(seconds)"
         
-        // if time runs out reset timer, display correct answer, load next round
+        // if time runs out reset timer, display correctness of answer
         if seconds == 0 {
             timer.invalidate()
             
@@ -237,33 +277,49 @@ class ViewController: UIViewController, WKNavigationDelegate {
         }
     }
     
-    // stop timer when answer is selected, hide label during feedback and reset counter
+    // stop timer when answer is submitted, hide timer label during feedback and reset seconds
     func hideTimer() {
         timer.invalidate()
         timerLabel.isHidden = true
         seconds = 60
+    }
+    
+    func loadWebsite(url: URL) {
+        // run when fact is tapped at end of round
+        let request = URLRequest(url: url)
+        webView = WKWebView(frame: self.view.frame)
+        webView.navigationDelegate = self
+        webView.load(request)
+        self.view.addSubview(webView)
+        self.view.sendSubview(toBack: webView)
+        
+        // hide stack view etc, show web view bar, hide status bar
+        hideItems()
+        webviewBar.isHidden = false
+        UIApplication.shared.keyWindow?.windowLevel = UIWindowLevelStatusBar
     }
 
     
     // MARK: - Actions
     
     @IBAction func factSwap(_ sender: UIButton) {
+        // behavior to swap facts when arrows are pressed, exchanging array indexes to maintain proper reference to each fact in array
         switch sender.tag {
-        case 1, 2:
+        case 1, 2: // swap between first and second fact
             (fact1Index, fact2Index) = (fact2Index, fact1Index)
             if let factArray = shuffledFacts {
 
                 fact1.setTitle(factArray[fact1Index].factText, for: .normal)
                 fact2.setTitle(factArray[fact2Index].factText, for: .normal)
             }
-        case 3, 4:
+        case 3, 4: // swap between second and third fact
             (fact2Index, fact3Index) = (fact3Index, fact2Index)
             if let factArray = shuffledFacts {
                 
                 fact2.setTitle(factArray[fact2Index].factText, for: .normal)
                 fact3.setTitle(factArray[fact3Index].factText, for: .normal)
             }
-        case 5, 6:
+        case 5, 6: // swap between third and fourth fact
             (fact3Index, fact4Index) = (fact4Index, fact3Index)
             if let factArray = shuffledFacts {
                 
@@ -275,24 +331,8 @@ class ViewController: UIViewController, WKNavigationDelegate {
         }
     }
     
-    // hide items to see web view
-    func hideItems() {
-        stackView.isHidden = true
-        resultButton.isHidden = true
-        learnMore.isHidden = true
-    }
-    
-    // show items again and hide web bar
-    func showItems() {
-        stackView.isHidden = false
-        resultButton.isHidden = false
-        learnMore.isHidden = false
-        webviewBar.isHidden = true
-    }
-    
-    
-    // go to web view
     @IBAction func factTap(_ sender: UIButton) {
+        // go to web view, loading index to access fact and its url property
         switch sender.tag {
         case 1:
             if let factArray = shuffledFacts {
@@ -317,28 +357,16 @@ class ViewController: UIViewController, WKNavigationDelegate {
         default: break
         }
     }
-    
-    func loadWebsite(url: URL) {
-        let request = URLRequest(url: url)
-        webView = WKWebView(frame: self.view.frame)
-        webView.navigationDelegate = self
-        webView.load(request)
-        self.view.addSubview(webView)
-        self.view.sendSubview(toBack: webView)
-        hideItems()
-        webviewBar.isHidden = false
-        UIApplication.shared.keyWindow?.windowLevel = UIWindowLevelStatusBar
-    }
-    
-    
+
     @IBAction func dismissWebView(_ sender: UIButton) {
+        // dismiss web view when web view bar is tapped, show stack view etc again, show status bar
         webView.removeFromSuperview()
         showItems()
-        UIApplication.shared.keyWindow?.windowLevel = UIWindowLevelNormal 
+        UIApplication.shared.keyWindow?.windowLevel = UIWindowLevelNormal
     }
     
-    // go to next round
     @IBAction func nextRound(_ sender: UIButton) {
+        // go to next round if six rounds have not been achieved, otherwise segue to results
         if rounds != 6 {
             resetRound()
         } else {
@@ -353,5 +381,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
             destinationViewController?.finalScore = correctAnswers
         }
     }
+    
+    
 }
 
